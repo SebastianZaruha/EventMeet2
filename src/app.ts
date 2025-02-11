46e008aa376335d12fb728fbe439b9c20af2fc7b
Jesus Repolles <jesusrepolles@gmail.com>
2025-02-11 17:54:05 +0100

    endpoint create y findInterestsByUserId de UserInterest

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { companyRouter } from "./routes/Company";
 import { eventRouter } from "./routes/Event";
 import { interestRouter } from "./routes/Interest";
 import { eventsInterestRouter } from "./routes/EventsInterest";
+import { userInterestRouter } from "./routes/UserInterest";
 import conectDB from "./config/db-connector";
 import "./models/Company";
 import "./models/Event";
@@ -46,6 +47,7 @@ app.use("/v1/companies", companyRouter);
 app.use("/v1/events", eventRouter);
 app.use("/v1/interests", interestRouter);
 app.use("/v1/events-interests", eventsInterestRouter);
+app.use("/v1/user-interests", userInterestRouter);
 // Si necesitas sincronizar todos los modelos
 (async () => {
   try {
